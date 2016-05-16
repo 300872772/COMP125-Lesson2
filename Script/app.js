67648b74e9df55c9aec62d08662e9528eef05c38
300872772 <mamun_131@hotmail.com>
2016-05-16 09:49:50 -0400

    Initial Commit

---
+++ Script/app.js
@@ -0,0 +1,36 @@
+/* main JavaScript file */
+
+
+
+//IIFE - Immidiately Invoked Function Expression
+
+
+(function(){
+    
+    "use strict";
+    
+//code goes in here    
+    consile.log ("App Started...")
+    
+    
+    
+    
+    
+    
+    
+    
+})();
+/*
+(function(){
+    
+   console.log("App Started"); 
+   
+   //variable decoration and Initialisation
+   var randomnumber = 0;
+   
+   randomnumber=Math.floor((Math.random()*10)+1);
+   
+   console.log(randomnumber);
+})();*/
+
+
